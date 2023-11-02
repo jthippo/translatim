aa16cb06e172de56a7e35ce8ab2c38a474b7724a
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-11-02 14:50:46 +0000

    end of codealong

--- server/server.js
+++ server/server.js
@@ -6,6 +6,33 @@ const app = express();
 app.use(cors());
 const axios = require("axios");
 
+// test our server is working properly
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
+
+// endpoint for main
 app.get("/", (_, response) => response.json("Root route for translatim."));
 
-app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
+// endpoint for translate
+app.get("/translate", async (request, response) => {
+  // long way without destructuring
+  // const word = request.query.word;
+  // const from = request.query.from;
+  // const to = request.query.to;
+
+  // quicker way, destructuring properties of request.query into variables
+  const { word, from, to } = request.query;
+
+  // api call
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  // axios makes its own get request from server to api
+  const res = await axios.get(API);
+
+  // wrangle only what we want
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+
+  // return wrangledData as json
+  response.json(wrangledData);
+});
