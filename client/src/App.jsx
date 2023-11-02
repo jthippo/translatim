0c20c846fa04e4e4babe3016f0a204c9b23c3892
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-11-02 15:37:19 +0000

    did some CSS, not enough time to start trying images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,12 @@ import axios from "axios";
 
 function App() {
   // store everything that changes in state
-  const [from, setFrom] = useState("ar");
-  const [to, setTo] = useState("ar");
+  const [from, setFrom] = useState("en");
+  const [to, setTo] = useState("es");
   const [word, setWord] = useState("");
   // translation is object because that's what's returned by server
   const [translation, setTranslation] = useState({});
+  const [accuracy, setAccuracy] = useState(0);
 
   // call the API to get translation
   async function handleTranslate(event) {
@@ -21,31 +22,38 @@ function App() {
   return (
     <>
       <form onSubmit={handleTranslate}>
-        <div className="container">
-          <select onChange={(event) => setFrom(event.target.value)}>
-            <option value="ar">Arabic</option>
-            <option value="en">English</option>
-            <option value="pl">Polish</option>
-            <option value="es">Spanish</option>
-            <option value="tr">Turkish</option>
-          </select>
-          <input
-            placeholder="Translate"
-            onChange={(event) => setWord(event.target.value)}
-          />
-        </div>
+        <div className="bigboy">
+          <div className="container">
+            <select onChange={(event) => setFrom(event.target.value)}>
+              <option value="ar">Arabic</option>
+              <option value="en" selected="selected">
+                English
+              </option>
+              <option value="pl">Polish</option>
+              <option value="es">Spanish</option>
+              <option value="tr">Turkish</option>
+            </select>
+            <textarea
+              className="input"
+              placeholder="Enter text"
+              onChange={(event) => setWord(event.target.value)}
+            />
+            <button>Translate</button>
+          </div>
 
-        <div className="container">
-          <select onChange={(event) => setTo(event.target.value)}>
-            <option value="ar">Arabic</option>
-            <option value="en">English</option>
-            <option value="pl">Polish</option>
-            <option value="es">Spanish</option>
-            <option value="tr">Turkish</option>
-          </select>
-          <span className="output">{translation.translation}</span>
+          <div className="container">
+            <select onChange={(event) => setTo(event.target.value)}>
+              <option value="ar">Arabic</option>
+              <option value="en">English</option>
+              <option value="pl">Polish</option>
+              <option value="es" selected="selected">
+                Spanish
+              </option>
+              <option value="tr">Turkish</option>
+            </select>
+            <div className="output">{translation.translation}</div>
+          </div>
         </div>
-        <button>Submit</button>
       </form>
     </>
   );
